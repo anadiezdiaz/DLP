9be6c32d8e0ae76c1fb88c5cc618705da1bcef2d
Ana <UO288302@uniovi.es>
2026-05-18 17:17:48 +0200

    arreglo fallo IdentificationVisitor

--- src/semantic/IdentificationVisitor.java
+++ src/semantic/IdentificationVisitor.java
@@ -35,12 +35,15 @@ public class IdentificationVisitor extends AbstractVisitor<Void, Void>{
     @Override
     public Void visit(Variable v, Void p) {
         Definition def = symbolTable.find(v.getName());
-        if(def != null){
-            v.setDefinition(def);
-        }else{
-            def = new VarDefinition(0,0, v.getName(),
-                new ErrorType(v.getLine(), v.getColumn(), "No VarDefinition associated to variable '" + v.getName() + "'"));
+
+        if (def == null) {
+            def = new VarDefinition(0, 0, v.getName(),
+                    new ErrorType(v.getLine(), v.getColumn(),
+                            "No VarDefinition associated to variable '" + v.getName() + "'"));
         }
+
+        v.setDefinition(def);
+
         super.visit(v, p);
         return null;
     }
